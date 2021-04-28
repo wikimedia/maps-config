99d77e9b58e46e360aa3a65f2654b457b69709e8
Alexander Rolek <a.rolek@gmail.com>
2021-04-28 09:19:45 -0600

    Generalized tables for layer land use
    * Updated imposm mapping file to introduce generalized (simplified)
    tables for the land use layer.
    * Updated the layer_landuse.sql function to take advantage of the new
    generalized tables.
    * Updated etl documentation around importing OSM data and SQL helper
    functions.
    * Added additional SQL helper functions.
    
    closes #6

--- etl/create_indexes.sql
+++ etl/create_indexes.sql
@@ -4,10 +4,16 @@ BEGIN;
 	CREATE INDEX ON admin (maritime);
 	CREATE INDEX ON admin (admin_level);
 
+	-- layer_landuse (need to add generalized tables as well)
+	--CREATE INDEX ON planet_osm_polygon_landuse(landuse)
+
 	-- layer_place_label
 	CREATE INDEX ON planet_osm_point (name);
 	CREATE INDEX ON planet_osm_point (place);
 	CREATE INDEX ON planet_osm_point (population);
+
+	-- layer_transportation_name
+	CREATE INDEX ON planet_osm_line(highway);
 
 	-- layer_water
 	CREATE INDEX ON planet_osm_polygon (landuse);
@@ -16,5 +22,6 @@ BEGIN;
 
 	-- layer_waterway
 	CREATE INDEX ON planet_osm_line (waterway);
-	CREATE INDEX ON planet_osm_polygon (natural);
+	CREATE INDEX ON planet_osm_polygon ("natural");
+
 COMMIT;
