814a1cff909ff38412d43be99f51cef3101faaad
mbsantos <msantos@wikimedia.org>
2021-05-10 16:58:52 +0200

    Fix layer_transportation_name_to_class. Despite the data is generated as MVT, it's not rendered by mapnik.

--- sql/functions/layer_transportation.sql
+++ sql/functions/layer_transportation.sql
@@ -1,99 +1,71 @@
-CREATE OR REPLACE FUNCTION public.layer_transportation(bbox geometry, zoom_level integer)
-    RETURNS TABLE(osm_id bigint, geometry geometry, class text, z_order integer, is text) 
+CREATE OR REPLACE FUNCTION public.layer_transportation_name(bbox geometry, zoom_level integer)
+    RETURNS TABLE(osm_id bigint, shield text, geometry geometry, name text, name_ text, class text, z_order integer, is text, ref text, reflen integer, len numeric)
     LANGUAGE 'sql'
     COST 100
     IMMUTABLE PARALLEL UNSAFE
     ROWS 1000
 
 AS $BODY$
-  SELECT 
-    osm_id, 
-    way, 
-    class,
-    z_order, 
-    "is" 
-  FROM (
-    SELECT
-        osm_id,
-        way,
-        CASE
-          WHEN highway IS NOT NULL THEN layer_transportation_name_to_class(highway, osm_id)
-          WHEN railway IS NOT NULL THEN layer_transportation_name_to_class(railway, osm_id)
-          WHEN access IS NOT NULL THEN layer_transportation_name_to_class(access, osm_id)
-          ELSE bail_out('Unexpected layer_transportation road row with osm_id=%s', osm_id::TEXT)
-        END AS class,
-        z_order,
-        CASE
-          -- maybe handle all these cases at the data import?
-          WHEN bridge IS NOT NULL AND bridge <> '' AND bridge <> 'no' AND bridge <> '0' THEN 'bridge'
-          WHEN tunnel IS NOT NULL AND tunnel <> '' AND tunnel <> 'no' AND tunnel <> '0' THEN 'tunnel'
-          ELSE 'road'
-        END AS "is"
-      FROM 
-        planet_osm_line
-      WHERE
-        (
-            (
-              highway IN ('motorway', -- 'motorway'
-                'primary', 'primary_link', 'trunk', 'trunk_link' -- 'main'
+  SELECT
+    *
+  FROM
+    (
+      SELECT
+          osm_id,
+          'default' AS shield,
+          way,
+          name,
+          (hstore_to_json(extract_names(tags)))::text name_,
+          layer_transportation_name_to_class(highway, railway, access, osm_id) AS class,
+          z_order,
+          CASE
+            -- maybe handle all these cases at the data import?
+            WHEN bridge IS NOT NULL AND bridge <> '' AND bridge <> 'no' AND bridge <> '0' THEN 'bridge'
+            WHEN tunnel IS NOT NULL AND tunnel <> '' AND tunnel <> 'no' AND tunnel <> '0' THEN 'tunnel'
+            ELSE 'road'
+          END AS "is",
+          ref,
+          pg_catalog.char_length(ref) AS reflen,
+          ROUND(Merc_Length(way)) AS len
+        FROM
+          planet_osm_line AS osm_line
+        WHERE
+          (
+              (
+                highway IN (
+                    'motorway', 'primary', 'primary_link', 'trunk',
+                    'trunk_link', 'secondary', 'secondary_link'
+                  )
+                AND (
+                  (name IS NOT NULL AND name <> '') OR (ref IS NOT NULL AND ref <> '')
+                )
+                AND zoom_level >= 11
               )
-              AND zoom_level >= 6
-            )
-            OR
-            ( -- 'main'
-              highway IN ('secondary', 'secondary_link')
-              AND zoom_level >= 9
-            )
-            OR
-            ( -- 'main'
-              highway IN ('tertiary', 'tertiary_link')
-              AND zoom_level >= 12
-            )
-            OR
-            ( -- 'street'
-              highway IN ('residential', 'unclassified', 'living_street')
-              AND zoom_level >= 12
-            )
-            OR
-            ( -- 'street_limited'
-              (highway IN ('pedestrian', 'construction') OR access = 'private')
-              AND zoom_level >= 12
-            )
-            OR
-            ( -- 'major_rail'
-              railway IN ('rail', 'monorail', 'narrow_gauge', 'subway', 'tram')
-              AND zoom_level >= 12
-            )
-            OR
-            ( -- 'motorway_link'
-              highway IN ('motorway_link')
-              AND zoom_level >= 13
-            )
-            OR
-            ( -- 'service'
-              highway IN ('service', 'track')
-              AND zoom_level >= 14
-            )
-            OR
-            ( -- 'driveway'
-              highway IN ('driveway')
-              AND zoom_level >= 14
-            )
-            OR
-            ( -- 'path'
-              highway IN ('path', 'cycleway', 'ski', 'steps', 'bridleway', 'footway')
-              AND zoom_level >= 14
-            )
-            OR
-            ( -- 'minor_rail'
-              railway IN ('funicular', 'light_rail', 'preserved')
-              AND zoom_level >= 14
-            )
+              OR
+              ( -- 'main'
+                highway IN (
+                    'tertiary', 'tertiary_link', 'residential', 'unclassified',
+                    'living_street', 'pedestrian', 'construction', 'rail', 'monorail',
+                    'narrow_gauge', 'subway', 'tram'
+                  )
+                AND (name IS NOT NULL AND name <> '')
+                AND zoom_level >= 12
+              )
+              OR
+              ( -- 'motorway_link'
+                highway IN (
+                    'motorway_link', 'service', 'track', 'driveway', 'path',
+                    'cycleway', 'ski', 'steps', 'bridleway', 'footway', 'funicular',
+                    'light_rail', 'preserved'
+                  )
+                AND (name IS NOT NULL AND name <> '')
+                AND zoom_level >= 14
+              )
           )
-        AND way && bbox
-    ) AS transportation
+          AND
+            way && bbox
+      ) AS transportation
     ORDER BY
       layer_transportation_priority_score(class, "is", z_order)
   ;
-
 $BODY$;
